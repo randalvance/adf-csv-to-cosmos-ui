afcfa7afc1619b9351d4e32e31057ce082060a53
Randal Vance Cunanan <randal@randalvance.net>
2021-12-30 22:25:40 +0800

    Add Uploader

---
+++ src/components/Uploader.vue
@@ -0,0 +1,17 @@
+<template>
+    <VueUploadComponent>
+        Upload File
+    </VueUploadComponent>
+</template>
+<script>
+import VueUploadComponent from 'vue-upload-component';
+
+export default {
+    components: {
+        VueUploadComponent
+    }
+}
+</script>
+<style lang="scss" scoped>
+
+</style>
