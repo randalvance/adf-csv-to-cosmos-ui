ef347afa4530d328ac14b99aa4a5d54d6378e52d
Randal Vance Cunanan <randal@randalvance.net>
2022-01-01 15:16:19 +0800

    Uploader

--- src/components/Uploader.vue
+++ src/components/Uploader.vue
@@ -1,17 +1,39 @@
 <template>
-    <VueUploadComponent>
-        Upload File
-    </VueUploadComponent>
+    <uploader :options="options" class="uploader">
+        <uploader-unsupport></uploader-unsupport>
+        <uploader-drop>
+            <p>Drop Files Here to Upload</p>
+            <uploader-btn>Select Files</uploader-btn>
+        </uploader-drop>
+    </uploader>
 </template>
 <script>
-import VueUploadComponent from 'vue-upload-component';
 
 export default {
-    components: {
-        VueUploadComponent
+    data: function() {
+        return {
+            options: {
+                target: 'https://localhost:7171/upload'
+            }
+        }
     }
 }
 </script>
-<style lang="scss" scoped>
-
+<style scoped>
+  .uploader {
+    width: 880px;
+    padding: 15px;
+    margin: 40px auto 0;
+    font-size: 12px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, .4);
+  }
+  .uploader .uploader-btn {
+    margin-right: 4px;
+  }
+  .uploader .uploader-list {
+    max-height: 440px;
+    overflow: auto;
+    overflow-x: hidden;
+    overflow-y: auto;
+  }
 </style>
