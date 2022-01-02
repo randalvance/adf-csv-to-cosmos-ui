1c455ff7153a4f9dbc3d5ec30eb950beca669c2d
Randal Vance Cunanan <randal@randalvance.net>
2022-01-02 15:03:40 +0800

    Optimizations

--- src/components/Uploader.vue
+++ src/components/Uploader.vue
@@ -1,43 +1,28 @@
 <template>
-    <uploader :options="options" class="uploader"
-              @upload-start="onUploadStart"
-              @complete="onComplete"
-              @file-error="onFileError"
-              @file-progress="onFileProgress"
-              ref="uploader">
+    <uploader :options="options" class="uploader">
         <uploader-drop>
             <p>Drop files here to upload or</p>
-            <uploader-btn single>Browse File</uploader-btn>
+            <uploader-btn :attrs="attrs" single>Browse File</uploader-btn>
         </uploader-drop>
         <uploader-list></uploader-list>
     </uploader>
 </template>
 <script>
 import config from '../config';
+import { v4 as uuid } from 'uuid';
 
 export default {
     data: function() {
         return {
-            isUploading: false,
-            error: '',
             options: {
-                target: `${config.backendUrl}/upload`
+                target: `${config.backendUrl}/upload`,
+                testChunks: false,
+                chunkSize: 10, // Upload in chunks of 10mb
+                generateUniqueIdentifier: () => uuid()
+            },
+            attrs: {
+                accept: '.csv'
             }
-        }
-    },
-    methods: {
-        onUploadStart() {
-            this.isUploading = true
-        },
-        onComplete() {
-            this.isUploading = false
-        },
-        onFileError(_rootFile, _file, message) {
-            this.isUploading = false
-            this.error = message
-        },
-        onFileProgress() {
-            console.log(this.$refs.uploader.uploader.progress())
         }
     }
 }
