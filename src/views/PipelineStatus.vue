845aaa27f16fcc9de9759d26f29b0e862b71f97c
Randal Vance Cunanan <randal@randalvance.net>
2022-01-02 16:17:04 +0800

    Polling

--- src/views/PipelineStatus.vue
+++ src/views/PipelineStatus.vue
@@ -7,15 +7,35 @@ import config from '../config';
 export default {
     data() {
         return {
-            pipelineStatus: 'Pending'
+            pipelineStatus: 'Pending',
+            intervalId: null
+        }
+    },
+    computed: {
+        pipelineRunId() {
+            return this.$route.params.pipelineRunId;
         }
     },
     async mounted() {
-        const pipelineRunId = this.$route.params.pipelineRunId;
-        const response = await fetch(`${config.backendUrl}/upload/pipeline-status/${pipelineRunId}`);
-        const result = await response.json();
-
-        this.pipelineStatus = result.status;
+        this.intervalId = setInterval(() => this.getStatus(), 3000);
     },
+    methods: {
+        async getStatus() {
+            try {
+                const response = await fetch(`${config.backendUrl}/upload/pipeline-status/${this.pipelineRunId}`);
+                const result = await response.json();
+                this.pipelineStatus = result.status;
+
+                if (this.pipelineStatus !== 'Pending') {
+                    clearInterval(this.intervalId);
+                }
+            } catch (error) {
+                clearInterval(this.intervalId);
+            }
+        }
+    },
+    destroyed() {
+        clearInterval(this.intervalId);
+    }
 }
 </script>
